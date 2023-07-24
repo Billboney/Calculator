891ddcc2f8c567433090efd1be71c96e76b7a536
Billboney <willharrison01@gmail.com>
2023-07-24 11:26:26 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,15 @@
+// <!--display-->
+// <!-- 0-9 numbers-->
+display = document.querySelector(".display")
+function press(){
+    //add number to display
+    display.remove
+}
+// <!-- . -->
+// <!-- ce -->
+// <!-- + -->
+// <!-- - -->
+// <!-- x -->
+// <!-- / -->
+// <!-- = -->
+// <!-- of/on-->
