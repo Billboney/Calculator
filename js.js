e2dc654e738f0b3fb00346979e439daa2f3271f7
Billboney <127686961+Billboney@users.noreply.github.com>
2023-08-12 18:44:24 +0100

    commit

--- js.js
+++ js.js
@@ -3,52 +3,100 @@
 display = document.querySelector(".display")
 tinyDisplay = document.querySelector(".tinyDisplay")
 i = 0
+storedValue = 0
 storedArray = []
 displayArray = []
 storedSym = ''
+value = ''
 
 function press(arg){
     displayArray.push(arg)
     display.removeChild(display.firstChild);
-    Value = displayArray.join('');
-    displayText = document.createTextNode(Value);
+    value = displayArray.join('');
+    displayText = document.createTextNode(value);
     display.appendChild(displayText);
-    return displayText, Value;
+    return value;
 }
 
-function calculate(displayText){
-    storedArray = []
+function clear(){
+
+}
+
+function storedsym(val){
+    storedSym = val
+    return storedSym;
+}
+
+function storedarray(val){
+    storedArray = val
+    return storedArray;
+}
+
+function storedvalue(val){
+    storedValue = val
+    return storedValue;
+}
+
+function update(storedValue){
+        displayText = document.createTextNode(storedValue)// display text is now added number
+        displayArray = [] //clears displayArray
+        display.removeChild(display.firstChild)//clear display
+        tinyDisplay.removeChild(tinyDisplay.firstChild);//both clear visual
+        display.textContent = 0;//sets visual to 0 to avoid null
+        tinyDisplay.appendChild(displayText);//put stored data on the back screen for visual
+        return;
+}
+
+function symbol(sym, storedArray, storedSym, storedValue){
+    console.log(storedValue)
+    console.log(sym)
+    symValue = parseInt(displayArray.join(''))// turn value from press into an integar
+    if (storedSym === '+'){
+        storedValue += symValue// add
+        update(storedValue)
+        storedvalue(storedValue)
+    }
+    else if (storedSym === '-'){
+        storedValue -= symValue // minus
+        update(storedValue)
+        storedvalue(storedValue)
+    }
+    else if (storedSym === '*'){
+        storedValue *= symValue
+        update(storedValue)
+        storedvalue(storedValue)
+    }
+    else if (storedSym === '/'){
+        storedValue /= symValue
+        update(storedValue)
+        storedvalue(storedValue)
+    }
+    else if (sym === '='){
+        tinyDisplay.removeChild(tinyDisplay.firstChild);
+        displayText = document.createTextNode(storedValue);
+        tinyDisplay.appendChild(displayText);
+        storedValue = 0
+        storedSym = '';
+        sym = ''
+        storedvalue(storedValue)
+        return storedSym;
+    }
+    else{
+    storedSym = sym
+    storedArray = []//clear stored array
     storedArray = displayArray //stores data off of display
     displayArray = [] //clears displayArray
-    display.removeChild(display.firstChild);
+    display.removeChild(display.firstChild)//clear display
     tinyDisplay.removeChild(tinyDisplay.firstChild);//both clear visual
-    display.textContent = 0;
+    display.textContent = 0;//sets visual to 0 to avoid null
     tinyDisplay.appendChild(displayText);//put stored data on the back screen for visual
-    return storedArray
-}
-
-function symbol(sym, Value, storedArray, storedSym){
-    storedValue = parseInt(storedArray.join(''))
-    symValue = parseInt(Value)
-   
-    if (storedSym = '+'){
-        symValue += storedValue
-        displayText = document.createTextNode(symValue)
-        console.log(symValue)
-        return displayText = document.createTextNode(symValue);
-    }
-    else if (sym = '-'){
-
-    }
-    else if (sym = '*'){
-
-    }
-    else if (sym = ''){
-
-    }
-    storedSym = sym
-
+    storedValue = parseInt(storedArray.join(''))//get stored value from array
+    storedarray(storedArray)
+    storedsym(storedSym)
+    storedvalue(storedValue)
+    console.log(storedValue)
     return storedSym;
+    }
 };
 
 
